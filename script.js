2cbb1cf54a1d12b866b2837bd837d2086e326ae4
lolabird <octavia@octavianichols.com>
2023-06-09 12:57:35 -0600

    fixed toc is not opening by default on desktop

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 // template-related scripts go here... 
-jQuery(function() {
+/*jQuery(function() {
     var $toc = jQuery('#dw__toc .toggle');
     if($toc.length) {
         $toc[0].setState(-1);
+    }
+});*/
+
+jQuery(function() {
+    var $tocHeader = jQuery('#dw__toc .toggle');
+    var $tocContent = $tocHeader.next(); // Assuming the contents of TOC are right after the toggle element
+    
+    if ($tocHeader.length && $tocContent.length) {
+        if (window.innerWidth >= 960) {
+            $tocContent.css('display', 'block');
+        } else {
+            $tocContent.css('display', 'none');
+        }
     }
 });
